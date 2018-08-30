8ec2aeb6fb57eaeca9b12d2d2e6a8de6a649bb9c
Sikder Tahsin Al-Amin <sal-amin2@uh.edu>
2018-08-30 10:44:43 -0500

    Update Paths_reachability.py

--- Paths_reachability.py
+++ Paths_reachability.py
@@ -4,31 +4,27 @@ import sys
 import math
 import time
  
- 
 ######connection string to vertica #####
 try:
-    conn_info = {'host':'192.168.1.11',
+    conn_info = {'host':'hostname/ip',
                  'port': 5433,
-                 'user': 'vertica',
-                 'password': '12512Marlive',
+                 'user': 'username',
+                 'password': 'password',
                  'database':'fournodes',
                  'read_timeout': 60000000,
                  'unicode_error': 'strict',
                  'ssl': False,
                  'connection_timeout': 50000
                  }
-  
     # simple connection, with manual close
     connection = vertica_python.connect(**conn_info)
     cur = connection.cursor()
- 
-  
+    
 except:
     print("Database connection error")
     sys.exit()
  
-sys.argv = ["seminaive_tc.py", "dataset=web-Google.csv","source=6"]
- 
+sys.argv = ["Paths_reacbability.py", "dataset=web-Google.csv","source=6"] ##This line is not needed when running from command line
  
 ####check the command line arguments ####
 if len(sys.argv) != 3:
@@ -45,9 +41,7 @@ source_vertex=arg3[1]
 
 dummy_table="test"
 path_length=6
-###initialize the variables####
 file = open("path_rechability.sql","w")
-#input_dataset='web-Google.csv'
  
 #####drop table######
 def drop_table(table_name):
@@ -58,12 +52,10 @@ def drop_table(table_name):
 for i in range(1,path_length+1):
     drop_table_name="P"+str(i)
     drop_table(drop_table_name)
-  
- 
+   
 ####Create E####
 drop_table('E')
-sql_string="CREATE TABLE E (i int, j int, v int);"
-#sql_string="CREATE TABLE E AS SELECT i as i, j AS j FROM "+dummy_table+";"
+sql_string="CREATE TABLE E (i int ENCODING RLE, j int ENCODING RLE, v int ENCODING RLE);"
 cur.execute(sql_string)
 file.write(sql_string+'\n')
  
@@ -73,7 +65,6 @@ sql_string="COPY E FROM '/home/vertica/tahsin/TC_programs/"+input_dataset+"' par
 cur.execute(sql_string)
 file.write(sql_string+'\n')
  
-
 #####Counting the number of triangles ######
 #drop_table('traingle_count')
 drop_table('P')
@@ -86,14 +77,13 @@ cur.execute(sql_string)
 print("Exploring Paths...")
 for i in range (2,path_length+1,1):
     sql_string="CREATE TABLE P"+str(i)+" AS SELECT P"+str(i-1)+".i AS i,E.j as j FROM P"+str(i-1)+" JOIN E ON P"+str(i-1)+".j=E.i GROUP BY P"+str(i-1)+".i, E.j;"
-    #sql_string="CREATE TABLE P"+str(i)+" AS SELECT P"+str(i-1)+".i AS i,E.j as j FROM P"+str(i-1)+" JOIN E ON P"+str(i-1)+".j=E.i; "   ###without groupby
+    #sql_string="CREATE TABLE P"+str(i)+" AS SELECT P"+str(i-1)+".i AS i,E.j as j FROM P"+str(i-1)+" JOIN E ON P"+str(i-1)+".j=E.i; "   ###without GROUPBY
     cur.execute(sql_string)
     #file.write(sql_string+'\n')
 
 #file.write('\n\n\n')
- 
- 
-print('creating final table...')
+
+print('Creating final table...')
 ###create the final path table
 drop_table('Reachability')
 sql_string="CREATE TABLE Reachability AS "
@@ -103,19 +93,17 @@ sql_string+=" SELECT * FROM P"+str(path_length)+";"
 cur.execute(sql_string)
 #file.write(sql_string+'\n')
  
-print('Total time=',time.time()-start_time)
+print('Total time=',time.time()-start_time)  ##total time 
 
 sql_string=" SELECT COUNT(*) FROM Reachability;"  
 cur.execute(sql_string)
 res=cur.fetchone()
 print("Total paths=",res)
 
-
 ##drop the TC tables
 file.write('\n')
 for i in range(1,path_length+1):
     drop_table("P"+str(i))
-
   
 file.close()
 connection.close()
